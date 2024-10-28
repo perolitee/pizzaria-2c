93181c7ca7ac0a6981aed48d7520b5d12f2f5d52
perolitee <perolaozacarioto@gmail.com>
2024-10-28 10:17:22 -0300

    aula navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,46 @@
 import logo from '../assets/react.svg'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 const Home = () => {
 
     return (
-        <img src={logo} width="300" 
-            alt="imagem do logo react" />
+        <>
+        <div>
+        <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
+        </div>
+
+        <h1>PROJETO PIZZARIA</h1>
+
+        <div>
+
+        </div>
+        </>
     )
 }
 
